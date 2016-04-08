6e748bd011d835ee35a273ebd749dd3a0260613b
S. Zarroug <szarroug3@gmail.com>
2016-04-08 15:10:33 -0500

    finished up shelfari_parser using lxml and regex

--- book/shelfari_parser.py
+++ book/shelfari_parser.py
@@ -1,24 +1,25 @@
 # shelfari_parser.py
 
-from HTMLParser import HTMLParser
-from urllib2 import *
+from urllib2 import urlopen
 from lxml import html
+import re
 
 # Parses shelfari page for characters, terms, and quotes
-class ShelfariParser(Object):
-	# global variables
+class ShelfariParser(object):
+    # global variables
+    _LETTERS_AND_NUMBERS = re.compile('([a-zA-Z0-9].+)')
 
-	def __init__(self, url):
-		response = urllib2.urlopen(url)
-		page_source = response.read()
-		self._html_source = html.fromstring(page_source)
-		self._characters = {}
-		self._terms = {}
-		self._quotes = []
+    def __init__(self, url):
+        response = urlopen(url)
+        page_source = response.read()
+        self._html_source = html.fromstring(page_source)
+        self._get_characters()
+        self._get_terms()
+        self._get_quotes()
 
-	@property
-	def characters(self):
-	    return self._characters
+    @property
+    def characters(self):
+        return self._characters
 
     @property
     def terms(self):
@@ -27,18 +28,29 @@ class ShelfariParser(Object):
     @property
     def quotes(self):
         return self._quotes
-	
-	def _get_characters(self):
-		'''get characters from sheflari page'''
-		charDesc = self._html_source.xpath('//div[@id="WikiModule_Characters"]//li//text()')
-		charName = self._html_source.xpath('//div[@id="WikiModule_Characters"]//li//span/text()')
-		self._characters = {char:desc for char, desc in zip(charName, charDesc)}
+    
+    def _get_characters(self):
+        '''get characters from sheflari page'''
+        self._characters = self._get_data_from_ul('//div[@id="WikiModule_Characters"]//ul[@class="li_6"]')
+        
 
-	def _get_terms(self):
-		'''get terms from shelfari page'''
+    def _get_terms(self):
+        '''get terms from shelfari page'''
+        self._terms = self._get_data_from_ul('//div[@id="WikiModule_Settings"]//ul[@class="li_6"]')
 
-	def _get_quotes(self):
-		'''get quotes from shelfari page'''
-		quoteList = self._html_source.xpath('//div[@id="WikiModule_Quotations"]//li//blockquote/text()')
-		self._quotes = [quote[1:-1] for quote in quoteList]
+    def _get_quotes(self):
+        '''get quotes from shelfari page'''
+        quoteList = self._html_source.xpath('//div[@id="WikiModule_Quotations"]//li//blockquote/text()')
+        self._quotes = [quote[1:-1] for quote in quoteList]
 
+    def _get_data_from_ul(self, xpath):
+        '''get data from a ul and put it into a dictionary'''
+        results = {}
+        ul = self._html_source.xpath(xpath)
+        for li in ul[0]:
+            label = li.getchildren()[0].text
+            labelAndDesc = li.xpath("string()")[len(label):]
+            descSearch = self._LETTERS_AND_NUMBERS.search(labelAndDesc)
+            desc = descSearch.group(1) if descSearch else None
+            results[label] = desc
+        return results
