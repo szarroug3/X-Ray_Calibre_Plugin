c17f6b5aed49478bc4782b71256a25e46a71f360
szarroug3 <szarroug3@gmail.com>
2016-05-22 10:51:15 -0500

    updated version in init and zip

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ class XRayCreatorPlugin(InterfaceActionBase):
     description         = 'A plugin to create X-Ray files for MOBI books'
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Samreen Zarroug & Alex Mayer'
-    version             = (2, 1, 0)
+    version             = (2, 1, 1)
     minimum_calibre_version = (2, 0, 0)
     actual_plugin       = 'calibre_plugins.xray_creator.ui:XRayCreatorInterfacePlugin'
 
